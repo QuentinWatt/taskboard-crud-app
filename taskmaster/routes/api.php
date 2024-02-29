125598f78e86369bc2106fdaf9df39b15be78f81
Quentin Watt <quentin@qcasts.com>
2024-02-29 23:27:24 +0200

    Add test for create

--- taskmaster/routes/api.php
+++ taskmaster/routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Api\v1\Boards\ShowBoardController;
 use App\Http\Controllers\Api\v1\Boards\ShowBoardsController;
+use App\Http\Controllers\Api\v1\Tasks\CreateBoardTaskController;
 use App\Http\Controllers\Api\v1\Tasks\ShowBoardTasksController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -23,5 +24,6 @@ Route::prefix('board')->group(function () {
         Route::get('/', ShowBoardController::class)->name('board.show');
 
         Route::get('/tasks', ShowBoardTasksController::class)->name('board.tasks.show');
+        Route::post('/task/new', CreateBoardTaskController::class)->name('board.task.create');
     });
 });
---
+++ taskmaster/tests/Feature/TaskTest.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use App\Models\Board;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class TaskTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     */
+    public function test_it_has_validation_on_an_empty_title(): void
+    {
+        $board = Board::factory()->create();
+        $response = $this->postJson('/api/board/' . $board->id . '/task/new', [
+            'title' => null,
+        ]);
+
+        $response->assertStatus(422);
+
+        $response->assertJsonFragment([
+            'message' => 'The title field is required.',
+            'errors' => [
+                'title' => [
+                    'The title field is required.'
+                ]
+            ],
+        ]);
+    }
+
+    public function test_it_has_validation_on_a_false_value(): void
+    {
+        $board = Board::factory()->create();
+        $response = $this->postJson('/api/board/' . $board->id . '/task/new', [
+            'title' => false,
+        ]);
+
+        $response->assertStatus(422);
+
+        $response->assertJsonFragment([
+            'message' => 'The title field must be a string. (and 1 more error)',
+            'errors' => [
+                'title' => [
+                    'The title field must be a string.',
+                    'The title field must be at least 1 characters.'
+                ]
+            ],
+        ]);
+    }
+
+    public function test_it_has_validation_on_long_value(): void
+    {
+        $board = Board::factory()->create();
+        $response = $this->postJson('/api/board/' . $board->id . '/task/new', [
+            'title' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat, quis. Quos minus aspernatur perferendis unde vero? Voluptas inventore totam consequatur neque debitis quasi ratione nostrum facilis, reprehenderit vitae ab distinctio.
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat, quis. Quos minus aspernatur perferendis unde vero? Voluptas inventore totam consequatur neque debitis quasi ratione nostrum facilis, reprehenderit vitae ab distinctio.
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat, quis. Quos minus aspernatur perferendis unde vero? Voluptas inventore totam consequatur neque debitis quasi ratione nostrum facilis, reprehenderit vitae ab distinctio.',
+        ]);
+
+        $response->assertStatus(422);
+
+        $response->assertJsonFragment([
+            'message' => 'The title field must not be greater than 255 characters.',
+            'errors' => [
+                'title' => [
+                    'The title field must not be greater than 255 characters.',
+                ]
+            ],
+        ]);
+    }
+
+    public function test_it_creates_a_task(): void
+    {
+        $board = Board::factory()->create();
+        $response = $this->postJson('/api/board/' . $board->id . '/task/new', [
+            'title' => 'Test Task',
+        ]);
+
+        $response->assertStatus(201);
+
+        $response->assertJsonFragment([
+            'title' => 'Test Task',
+            'is_completed' => false
+        ]);
+    }
+}
