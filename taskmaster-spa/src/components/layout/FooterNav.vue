98ef58067418312a2916aa4c7af7ea445f2b0f41
Quentin Watt <quentin@qcasts.com>
2024-03-01 15:05:48 +0200

    Add Vue Router and some basic page components

---
+++ taskmaster-spa/src/components/layout/FooterNav.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+  const date = new Date();
+</script>
+
+<template>
+  <div class="bg-gray-800 text-white flex justify-center py-5">
+
+    <div class="text-center text-sm">
+      &copy; copyright {{ date.getFullYear() }}
+    </div>
+  </div>
+</template>
---
+++ taskmaster-spa/src/pages/Home.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+// import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div class="container mx-auto py-5">
+    <h1 class="text-2xl font-bold">
+      Home page
+    </h1>
+  </div>
+</template>
