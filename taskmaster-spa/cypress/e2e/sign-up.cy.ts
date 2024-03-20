f27610495de290097c2b63793f53b89c7de079b5
Quentin Watt <quentin@qcasts.com>
2024-03-20 09:42:23 +0200

    Check for sign up form

--- taskmaster-spa/cypress/e2e/sign-up.cy.ts
+++ taskmaster-spa/cypress/e2e/sign-up.cy.ts
@@ -1,10 +1,11 @@
 describe('It has a sign-up page', () => {
   const baseUrl = Cypress.config('baseUrl')
 
-  it('Should return an error the wrong details are used.', () => {
+  it('It has the correct h1 and sign up form', () => {
     cy.visit(`${baseUrl}signup`); 
     cy.url().should('eq', `${baseUrl}signup`)
-
-    // cy.contains('h1', 'Sign Up').should('exist');
+    cy.contains('h1', 'Sign Up').should('exist');
+    cy.get('form[data-cy="sign-up-form"]')
+      .should('exist');
   });
 })
