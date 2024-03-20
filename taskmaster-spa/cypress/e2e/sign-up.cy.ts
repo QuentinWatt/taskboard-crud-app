d29b794175d950ad9c0a39a8ba6658bb636f84c8
Quentin Watt <quentin@qcasts.com>
2024-03-20 09:49:57 +0200

    Find form on sign up page

--- taskmaster-spa/cypress/e2e/sign-up.cy.ts
+++ taskmaster-spa/cypress/e2e/sign-up.cy.ts
@@ -2,9 +2,12 @@ describe('It has a sign-up page', () => {
   const baseUrl = Cypress.config('baseUrl')
 
   it('It has the correct h1 and sign up form', () => {
-    cy.visit(`${baseUrl}signup`); 
-    cy.url().should('eq', `${baseUrl}signup`)
-    cy.contains('h1', 'Sign Up').should('exist');
+    cy.visit(`${baseUrl}sign-up`); 
+    cy.url().should('eq', `${baseUrl}sign-up`)
+
+    cy.get('h1').should('exist').then((element) => {
+      expect(element.html()).to.contain('Sign Up')
+    });
     cy.get('form[data-cy="sign-up-form"]')
       .should('exist');
   });
