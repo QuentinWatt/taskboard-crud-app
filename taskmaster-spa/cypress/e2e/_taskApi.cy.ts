53ea6fff988f81da540cde4249b95c8040cc8aad
Quentin Watt <quentin@qcasts.com>
2024-03-20 16:16:46 +0200

    add cypress

--- taskmaster-spa/cypress/e2e/_taskApi.cy.ts
+++ taskmaster-spa/cypress/e2e/_taskApi.cy.ts
@@ -2,6 +2,13 @@ describe('it can reach the back end', () => {
   const apiUrl = Cypress.env('apiUrl')
 
   it('can visit the laravel app', () => {
+    cy.request(`${apiUrl}api/test`).then((response) => {
+      expect(response.status).to.eq(200);
+      expect(response.body).to.have.property('message', 'up');
+    })
+  })
+
+  it('can visit the laravel app', () => {
     cy.visit(apiUrl).then(() => {
       cy.contains('h2', 'Documentation').should('exist');
     });
